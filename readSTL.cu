cbcd6b14e7dabe9a6e11d744f0786bdfcd1623cf
makanicb <makanicb25@gmail.com>
2024-12-29 12:51:14 -0800

    Read STL file completed

--- readSTL.cu
+++ readSTL.cu
@@ -21,6 +21,35 @@ int readBinarySTL (char *filename)
 	//print the values in the buffers
 	std::cout << header << std::endl;
 	std::cout << numTri << std::endl;
+	//read the triangles
+	//create buffers for the normal vector, position vectors
+	//for the three vertices of each triangle, and attribute
+	//byte count
+	float normal[3];
+	float vert1[3];
+	float vert2[3];
+	float vert3[3];
+	short attr;
+	for (int i = 0; i < numTri; i++)
+	{
+		//read into the buffers
+		fread(normal, 4, 3, f);
+		fread(vert1, 4, 3, f);
+		fread(vert2, 4, 3, f);
+		fread(vert3, 4, 3, f);
+		fread(&attr, 2, 1, f);
+		//print out the values in the buffers
+		std::cout << "N\tV1\tV2\tV3" << std::endl;
+		for(int j = 0; j < 3; j++)
+		{
+			std::cout << normal[j] << "\t";
+			std::cout << vert1[j] << "\t";
+			std::cout << vert2[j] << "\t";
+			std::cout << vert3[j] << "\t";
+			std::cout << std::endl;
+		}
+		std::cout << "Attribute Byte Count: " << attr << std::endl;
+	}
 	//close the file
 	fclose(f);
 	return 0;
