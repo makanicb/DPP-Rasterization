433d7bc4ac60aa8596448d985995b319d6db276b
makanicb <makanicb25@gmail.com>
2025-06-29 11:39:11 -0700

    Use viskores in rasterize header

--- rastByTri.h
+++ rastByTri.h
@@ -3,9 +3,9 @@
 #include <thrust/device_vector.h>
 #include "imageWriter.h"
 
-void RasterizeTriangles(thrust::device_vector<thrust::tuple<float, float, float>> &p1,
-		thrust::device_vector<thrust::tuple<float, float, float>> &p2,
-		thrust::device_vector<thrust::tuple<float, float, float>> &p3,
-		thrust::device_vector<thrust::tuple<char, char, char>> &color,
+void RasterizeTriangles(viskores::cont::ArrayHandle<thrust::tuple<float, float, float>> &p1,
+		viskores::cont::ArrayHandle<thrust::tuple<float, float, float>> &p2,
+		viskores::cont::ArrayHandle<thrust::tuple<float, float, float>> &p3,
+		viskores::cont::ArrayHandle<thrust::tuple<char, char, char>> &color,
 		int numTri, int width, int height, Image &final_image);
 #endif
