7e8987df1527a5e693dfb8353586da130e9ff038
Makani Buckley <makanib@login09.chn.perlmutter.nersc.gov>
2025-09-26 13:36:56 -0700

    Updated to make size of arrays variable

--- test_basics/test_add_array.cu
+++ test_basics/test_add_array.cu
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <functional>
+#include <cmath>
 
 #include <viskores/cont/Algorithm.h>
 #include <viskores/cont/ArrayHandle.h>
 #include <viskores/cont/Initialize.h>
+#include <viskores/cont/Timer.h>
+
+#ifndef EXP
+#define EXP 8
+#endif
 
 int main(int argc, char **argv){
 
+	//Initialize program
 	viskores::cont::Initialize(argc, argv, viskores::cont::InitializeOptions::AddHelp);
-	const int ARRAY_SIZE = 1 << 32;
+	const int ARRAY_SIZE = (int) pow(2, EXP);
+
+	//Initialize and start timer
+	viskores::cont::Timer timer;	
+	timer.Start();
 
 	//Initialize arrays
 	viskores::cont::ArrayHandle<int> arr1;
@@ -19,6 +30,13 @@ int main(int argc, char **argv){
 	//Take the sum of the arrays
 	viskores::cont::ArrayHandle<int> sum;
 	viskores::cont::Algorithm::Transform(arr1, arr2, sum, std::plus<int>());
+
+	//Stop timer
+	timer.Stop();
+
+	//Print runtime
+	std::cout << "Time (microseconds) to add arrays (len=" << ARRAY_SIZE <<
+		"): " << timer.GetElapsedTime() * 1e6 << std::endl;
 
 	return 0;
 }
