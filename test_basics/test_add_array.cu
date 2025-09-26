8397def0e5c9cbf9f5573c5dd9e8d9a00f3a4f6c
Makani Buckley <makanib@login09.chn.perlmutter.nersc.gov>
2025-09-26 11:56:31 -0700

    Finished basic test and created cmake files

--- test_basics/test_add_array.cu
+++ test_basics/test_add_array.cu
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <functional>
 
-#include <viskores/cont/ArrayHandle>
+#include <viskores/cont/Algorithm.h>
+#include <viskores/cont/ArrayHandle.h>
+#include <viskores/cont/Initialize.h>
 
 int main(int argc, char **argv){
 
+	viskores::cont::Initialize(argc, argv, viskores::cont::InitializeOptions::AddHelp);
 	const int ARRAY_SIZE = 1 << 32;
 
 	//Initialize arrays
@@ -15,7 +18,7 @@ int main(int argc, char **argv){
 
 	//Take the sum of the arrays
 	viskores::cont::ArrayHandle<int> sum;
-
+	viskores::cont::Algorithm::Transform(arr1, arr2, sum, std::plus<int>());
 
 	return 0;
 }
