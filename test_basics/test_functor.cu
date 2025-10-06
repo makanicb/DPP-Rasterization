7cf3ab634e78c0fee1fc2d5ff27a7d8d71cd9a95
Makani Buckley <makanib@login35.chn.perlmutter.nersc.gov>
2025-10-06 12:58:49 -0700

    thrust warm up

--- test_basics/test_functor.cu
+++ test_basics/test_functor.cu
@@ -30,33 +30,39 @@ int main(int argc, char **argv)
 	//Initialize program
 	const int ARRAY_SIZE = pow(2, EXP);
 
-	//Initialize timer
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
+	for(int i = 0; i < 2; i++)
+	{
+		//Initialize timer
+		cudaEvent_t start, stop;
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
 
-	//Start timer
-	cudaEventRecord(start);
+		//Start timer
+		cudaEventRecord(start);
 
-	//Initialize arrays
-	thrust::device_vector<int> arr1(ARRAY_SIZE);
-	thrust::device_vector<int> arr2(ARRAY_SIZE);
-	thrust::fill(arr1.begin(), arr1.end(), 256);
-	thrust::fill(arr2.begin(), arr2.end(), 1024);
+		//Initialize arrays
+		thrust::device_vector<int> arr1(ARRAY_SIZE);
+		thrust::device_vector<int> arr2(ARRAY_SIZE);
+		thrust::fill(arr1.begin(), arr1.end(), 256);
+		thrust::fill(arr2.begin(), arr2.end(), 1024);
 
-	//Add arrays
-	thrust::device_vector<int> sum(ARRAY_SIZE);
-	thrust::transform(arr2.begin(), arr2.end(), arr1.begin(), sum.begin(), expensiveFunctor());
+		//Add arrays
+		thrust::device_vector<int> sum(ARRAY_SIZE);
+		thrust::transform(arr2.begin(), arr2.end(), arr1.begin(), sum.begin(), expensiveFunctor());
 
-	//Stop timer
-	cudaEventRecord(stop);
+		//Stop timer
+		cudaEventRecord(stop);
 
-	//Print runtime
-	cudaEventSynchronize(stop);
-	float runtime_ms = 0;
-	cudaEventElapsedTime(&runtime_ms, start, stop);
-	std::cout << "Time (microseconds) to add two arrays (len=" << ARRAY_SIZE
-		<< "): " << runtime_ms * 1e3 << std::endl;
+		if(i == 1)
+		{
+			//Print runtime
+			cudaEventSynchronize(stop);
+			float runtime_ms = 0;
+			cudaEventElapsedTime(&runtime_ms, start, stop);
+			std::cout << "Time (microseconds) to add two arrays (len=" << ARRAY_SIZE
+				<< "): " << runtime_ms * 1e3 << std::endl;
+		}
+	}
 
 	return 0;
 }
