82c9365916e6ddb8a6fa4ac5d7d86cf2a2b7ffd4
Makani Buckley <makanib@login13.chn.perlmutter.nersc.gov>
2025-12-17 10:24:15 -0800

    Added scalability test + updated CMAKE list to find viskores

---
+++ SCALABILITY_TEST_GPU.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+read -p "File: " fin
+
+base=$(basename $fin .stl)
+fout="img/${base}.pnm"
+echo $fin
+echo $fout
+
+./triCount $fin
+
+echo -e "Scale, Rasterize, Sort, Select, Write"
+
+for i in 1 2 4 8 16
+do
+	echo -n -e "$i\t$mult"
+	build_thrust/rast $fin $fout $i
+done
+
+echo -e "Subdivisions, Rasterize, Sort, Select, Write"
+
+for i in 0 1 2 3 4 5 6
+do
+	echo -n -e "$i"
+	build_thrust/rast $fin $fout 1 $i
+done
