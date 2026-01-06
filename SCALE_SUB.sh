acdc444819a58fc3bc62d393bc1b1e3c8812dbd5
Makani Buckley <makanib@login07.frontier.olcf.ornl.gov>
2026-01-05 20:35:54 -0500

    Modified scale_sub.sh to be full test

--- SCALE_SUB.sh
+++ SCALE_SUB.sh
@@ -7,16 +7,15 @@ fout="img/${base}.pnm"
 echo $fin
 echo $fout
 
-./triCount $fin
+#./triCount $fin
 
-echo -e "Scale\tTriangle Multiplier\tRasterize\tSort\tSelect\tWrite"
+echo "Scale, Subdivisions, Rasterize, Runtime"
 
-for i in 1 2 4 8 16
+for i in 1 2 4 8 16 32 64 128
 do
-	for j in 0 1 2 3 4
+	for j in 0 1 2 3 4 5 6
 	do
-		let mult=4**$j
-		echo -n -e "$i\t$mult"
-		build/rast --viskores-device CUDA $fin $fout $i $j
+		echo -n "$i, $j"
+		build_viskores/rast --viskores-device=Kokkos $fin $fout $i $j
 	done
 done
